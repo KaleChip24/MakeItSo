9a4963e025bf072516bde9f151e9b2618c4ee148
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-11-08 11:55:36 -0500

    html bones. start to js pseudo code

---
+++ Script.js
@@ -0,0 +1,10 @@
+// need a fetchData function
+// need an event listener on the button
+// need event listened to delete previous search
+// create and append img for builds
+// create the inner text
+//  display build name
+// display build link
+// make sure to console log everything so that you know if it's working
+
+//
