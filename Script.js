fa32274bae82f43fa20293b0c6f40690b0aee527
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-11-08 16:05:17 -0500

    getting an error message about my eventlistener

--- Script.js
+++ Script.js
@@ -1,22 +1,29 @@
 // create variables that are being listened to
-const searchBar = document.querySelector("#search-bar");
+const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#moc-search");
+//create button variable
+const button = document.querySelector("#getMoc");
 
 // need a fetchData function with axios
 // remember async await needs a try and a catch
-async function fetchBuilds() {
+async function fetchBuilds(set_num) {
+  const url = `https://rebrickable.com/api/v3/lego/sets/${set_num}/alternates/?key=430949585e7e48d558a057f45e6f437f`
   try {
-    const url = `https://rebrickable.com/api/v3/lego/sets/{set_num}/alternates/?key=430949585e7e48d558a057f45e6f437f`
     const res = await axios.get(url)
-    console.log(res)
+    const bricksData = res.data.results
+    console.log(res.data.results)
   } catch (error) {
     console.log(error)
   }
+  // console.log(res)
 }
+
+
 // need an event listener on the button
-searchBar.addEventListener("submit", () => {
-  const set_num = searchInput;
-  console.log(searchInput)
+searchForm.addEventListener("submit", () => {
+  // let set_num = searchInput.value;
+  console.log(searchInput.value);
+  fetchBuilds(searchInput.value);
 })
 // need event listened to delete previous search
 // create and append img for builds
@@ -25,5 +32,3 @@ searchBar.addEventListener("submit", () => {
 // display build link
 // make sure link is clickable! or that you can add a copy paste window
 // make sure to console log everything so that you know if it's working
-
-//
