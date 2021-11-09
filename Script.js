6eccb90015b2884c8d1f491d9382d4c8f09723a9
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-11-09 12:36:02 -0500

    info is populating

--- Script.js
+++ Script.js
@@ -10,8 +10,11 @@ async function fetchBuilds(set_num) {
   try {
     const url = `https://rebrickable.com/api/v3/lego/sets/${set_num}/alternates/?key=430949585e7e48d558a057f45e6f437f`;
     const res = await axios.get(url);
-    const bricksData = res.data
-    console.log(res.data.results)
+    if (res.data.results) {
+      const bricksData = res.data.results
+      showBuildList(bricksData)
+      console.log(bricksData)
+    }
   } catch (error) {
     console.log(error)
   }
@@ -30,9 +33,25 @@ searchForm.addEventListener("submit", (e) => {
 
 
 // need event listened to delete previous search
-// create and append img for builds
-// create the inner text
-// display build name
-// display build link
-// make sure link is clickable! or that you can add a copy paste window
-// make sure to console log everything so that you know if it's working
+
+const mocOptions = document.querySelector("#mocOptions");
+
+function showBuildList(builds) {
+  builds.forEach(build => {
+    const buildTitle = document.createElement("h4")
+    buildTitle.innerText = `${build.designer_name}`
+    mocOptions.appendChild(buildTitle)
+    const buildImg = document.createElement("img")
+    buildImg.src = build.moc_img_url;
+    mocOptions.appendChild(buildImg);
+  })
+
+
+  // create and append img for builds
+  // create the inner text
+  // display build name
+  // display build link
+  // make sure link is clickable! or that you can add a copy paste window
+  // make sure to console log everything so that you know if it's working
+  // console.log(buildImg)
+}
