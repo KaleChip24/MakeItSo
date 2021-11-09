5abc88e4c9809c8e5bd5e6674f608aeff5b0327f
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-11-09 10:52:19 -0500

    api working and pulling in info from specific set num

--- Script.js
+++ Script.js
@@ -7,24 +7,27 @@ const button = document.querySelector("#getMoc");
 // need a fetchData function with axios
 // remember async await needs a try and a catch
 async function fetchBuilds(set_num) {
-  const url = `https://rebrickable.com/api/v3/lego/sets/${set_num}/alternates/?key=430949585e7e48d558a057f45e6f437f`
   try {
-    const res = await axios.get(url)
-    const bricksData = res.data.results
+    const url = `https://rebrickable.com/api/v3/lego/sets/${set_num}/alternates/?key=430949585e7e48d558a057f45e6f437f`;
+    const res = await axios.get(url);
+    // const bricksData = res.data.results
     console.log(res.data.results)
   } catch (error) {
     console.log(error)
   }
-  // console.log(res)
 }
 
+fetchBuilds("75936-1")
 
-// need an event listener on the button
-searchForm.addEventListener("submit", () => {
-  // let set_num = searchInput.value;
-  console.log(searchInput.value);
-  fetchBuilds(searchInput.value);
-})
+
+// // need an event listener on the button
+// searchForm.addEventListener("submit", () => {
+//   let set_num = searchInput.value;
+//   console.log(searchInput);
+//   fetchBuilds(searchInput);
+// })
+
+
 // need event listened to delete previous search
 // create and append img for builds
 // create the inner text
