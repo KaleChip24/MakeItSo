9c253a3c001f3f34eb8a231e87197773f2758285
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-11-12 13:26:44 -0500

    links have new features like hover and color change

--- Script.js
+++ Script.js
@@ -74,21 +74,8 @@ function removeBuild() {
   }
 }
 
-// Favorite Button - Heart
-$('.favme').click(function () {
-  $(this).toggleClass('active');
-});
-
-/* when a user clicks, toggle the 'is-animating' class */
-$(".favme").on('click touchstart', function () {
-  $(this).toggleClass('is_animating');
-});
-
-/*when the animation is over, remove the class*/
-$(".favme").on('animationend', function () {
-  $(this).toggleClass('is_animating');
-});
 
 function addFav() {
   const favBtn = document.createElement("button")
+
 }
